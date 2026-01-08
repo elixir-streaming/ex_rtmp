27b14eed7a53491959d4fdd9249ab7de3a5be223
Billal GHILAS <bilal@evercam.io>
2026-01-08 22:09:20 +0100

    Bump min elixir version to 1.16

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExRTMP.MixProject do
     [
       app: :ex_rtmp,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
